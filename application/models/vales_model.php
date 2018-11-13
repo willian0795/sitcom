895a3175efcdde237eba1902bbe15de5c2477ccd
Alberto S. Castaneda <alb2594@gmail.com>
2018-11-13 09:28:53 -0600

    cambios consulta de reporte por eliminacion de tabla liquidacion planta

--- application/models/vales_model.php
+++ application/models/vales_model.php
@@ -1076,8 +1076,8 @@ FROM (SELECT lc.id_seccion, lc.mes_liquidacion AS mes, SUM(lc.sobrantes_anterior
 	        	COALESCE(SUM(lc.entregados),0) AS asignado,  SUM(lc.disponibles) AS disponibles, 
 				COALESCE(SUM(lc.consumidos),0) AS consumidos, SUM(lc.sobrantes_despues) sobrantes_despues,
 				CONCAT(s.nombre_seccion) seccion 
-		FROM (SELECT * FROM tcm_liquidacion_combustible  UNION SELECT id_liquidacion_planta AS id_liquidacion_combustible, 0 AS id_seccion, mes_liquidacion, 0 AS sobrantes_anterior, entregados, entregados AS disponibles, entregados AS consumidos, 0 sobrantes_despues, id_fuente_fondo  FROM tcm_liquidacion_planta) lc 
-		JOIN (SELECT * FROM org_seccion UNION SELECT 0, 'PLANTA ELECTRICA',0,1) s ON s.id_seccion = lc.id_seccion 
+		FROM (SELECT * FROM tcm_liquidacion_combustible) lc 
+		JOIN (SELECT * FROM org_seccion UNION SELECT *,1 FROM tcm_seccion_adicional) s ON s.id_seccion = lc.id_seccion 
 	    WHERE lc.mes_liquidacion BETWEEN DATE_FORMAT(DATE('".$fecha_inicio."'),'%Y%m') 
 	        	AND DATE_FORMAT(DATE('".$fecha_fin."'),'%Y%m') ".$queryadds."
 	    GROUP BY lc.mes_liquidacion, lc.id_seccion) AS q2
@@ -1089,7 +1089,7 @@ LEFT JOIN (SELECT s.id_seccion, SUM( cv.cantidad_vales) consumido, v.valor_nomin
 			INNER JOIN tcm_requisicion_vale rv ON rv.id_requisicion_vale = rvcv.id_requisicion_vale
 			INNER JOIN tcm_requisicion r ON r.id_requisicion = rv.id_requisicion
 			INNER JOIN tcm_requisicion_vale2 rv2 ON r.id_requisicion = rv2.id_requisicion
-			INNER JOIN (SELECT * FROM org_seccion UNION SELECT 0, 'PLANTA ELECTRICA',0,1) s ON s.id_seccion= r.id_seccion
+			INNER JOIN (SELECT * FROM org_seccion UNION SELECT *,1 FROM tcm_seccion_adicional) s ON s.id_seccion= r.id_seccion
 			INNER JOIN tcm_vale v ON v.id_vale  = rv.id_vale
 			INNER JOIN tcm_fuente_fondo f ON f.id_fuente_fondo = r.id_fuente_fondo
 		WHERE fecha_factura BETWEEN '".$fecha_inicio."' AND '".$fecha_fin."' ".$queryadds2."
